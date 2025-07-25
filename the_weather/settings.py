1f5812488b5c0c5b1f6018d94d30f486ff517e18
fede1975 <federicorizzato75@gmail.com>
2025-07-25 12:36:17 +0200

    fase sbffds

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -16,6 +16,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -115,7 +118,7 @@ USE_TZ = True
 
 
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
